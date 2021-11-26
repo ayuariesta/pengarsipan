ebd7ceba890a46cec5bcd14dc9db6edb9b0d1e03
= <=>
2021-11-26 08:35:51 +0700

    ubah line 18

--- app/Http/Controllers/SuratController.php
+++ app/Http/Controllers/SuratController.php
@@ -15,6 +15,7 @@ class SuratController extends Controller
 
         $data_surat = DB::table('surat')
         ->orderByRaw('created_at DESC')
+        ->whereRaw("1=2")
         ->paginate(10);
 
         
